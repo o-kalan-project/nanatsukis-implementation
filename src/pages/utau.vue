bcbf04a9874952de5928f255cf4172627e674f10
creeper-0910 <56744841+creeper-0910@users.noreply.github.com>
2024-07-16 10:21:13 +0900

    markdownを直書きに

--- src/pages/utau.vue
+++ src/pages/utau.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
-import Markdown from '@/components/Markdown.vue';
-import utau from '@/contents/utau.md?raw';
 defineExpose({
   title: "UTAU音源",
 });
+const utauDL = () => {
+  location.href = "https://github.com/o-kalan-project/o-ka-utau/releases/latest/download/o-kalan.zip"
+} 
 </script>
 <template>
   <section class="relative flex h-full">
     <div class="p-4 w-full lg:w-2/3 flex-col mx-auto">
       <h1 class="page-title">UTAU音源</h1>
-      <Markdown :source="utau" />
+      配布してるよ～（してない）
+      <br>
+      <button disabled @click="utauDL">ダウンロード</button>
     </div>
   </section>
 </template>
